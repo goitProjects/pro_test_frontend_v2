4d0cb9122ccb9afeea9f0fd1472b461e3c897064
z9877969 <z9877969@ukr.net>
2023-03-29 15:59:26 +0300

    changes langToggleOptions file

--- src/options/langToggleOptions.js
+++ src/options/langToggleOptions.js
@@ -1,8 +1,5 @@
 import { langValuesOptions } from "./langValuesOptions";
 
 export const options = Object.values(langValuesOptions).map(
-  ({ name, title }) => ({
-    value: name,
-    label: title,
-  })
+  ({ name, title }) => ({ value: name, label: title })
 );
